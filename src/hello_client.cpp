3d9eddf94ecebed9c048df92a43791a65e61d56d
epqlffltm <kjh55984249@gmail.com>
2026-02-15 10:50:59 +0900

    오타 수정

--- src/hello_client.cpp
+++ src/hello_client.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[])
     exit(1);
   }
 
-  serv = socket(PF_INET, SOCK_STREAM, 0);
+  sock = socket(PF_INET, SOCK_STREAM, 0);
   if(sock == -1)
   {
     error_handling("socket error");
