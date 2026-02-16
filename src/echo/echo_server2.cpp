a4620565a42995c38da1a26dde649ead042fad14
epqlffltm <kjh55984249@gmail.com>
2026-02-16 12:04:57 +0900

    에코 서버 재작성(연습)

---
+++ src/echo/echo_server2.cpp
@@ -0,0 +1,75 @@
+/*
+2026-02-16
+에코 서버 재작성(연습)
+*/
+
+#include<iostream>
+#include<cstdlib>
+#include<cstring>
+#include <unistd.h>
+#include<vector>
+#include<arpa/inet.h>
+#include<sys/socket.h>
+#include"../error_handling.h"
+
+int main(int argc, char*argv[])
+{
+  int clnt_sock = -1, str_len = 0, opt = 1;
+  const int buf_size = 1024;
+  std::vector<char>message(buf_size);
+
+  struct sockaddr_in serv_adr, clnt_adr;
+  socklen_t clnt_adr_sz;
+
+  if(argc != 2)
+  {
+    std::cerr << "useage: " << argv[0] << "<port>" << std::endl;
+    exit(1);
+  }
+
+  int serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+  if(serv_sock == -1)
+  {
+    error_handling("socket() error");
+  }
+
+  memset(&serv_adr, 0, sizeof(serv_adr));//주소 구조체 활성화
+  serv_adr.sin_family = AF_INET;//IPv4
+  serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);//내컴푸터 ip 할당
+  serv_adr.sin_port = htons(atoi(argv[1]));//포트번호 할당
+
+  setsockopt(serv_sock, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+
+  if(bind(serv_sock,(struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
+  {
+    error_handling("bond");
+  }
+
+  if(listen(serv_sock, 5) == -1)
+  {
+    error_handling("listen");
+  }
+
+  clnt_adr_sz = sizeof(clnt_adr);
+
+  for(int i = 0; i < 5; i++)
+  {
+    clnt_sock = accept(serv_sock, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
+    if(clnt_sock == -1)
+    {
+      error_handling("accept");
+    }
+    else
+    {
+      std::cout << "connected client" << i + 1 << std::endl;
+    }
+    while((str_len = read(clnt_sock, message.data(), buf_size)) != 0)
+    {
+      write(clnt_sock, message.data(), str_len);
+    }
+    close(clnt_sock);
+  }
+  close(serv_sock);
+
+  return 0;
+}
