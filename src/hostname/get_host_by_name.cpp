98de696195300ae222a7a77adaddcb4aed0c7edc
epqlffltm <kjh55984249@gmail.com>
2026-02-23 18:21:55 +0900

    get_host_by_name error path

--- src/hostname/get_host_by_name.cpp
+++ src/hostname/get_host_by_name.cpp
@@ -5,19 +5,18 @@
 
 #include<iostream>
 #include<cstdlib>
-#include<cstring>
-#include<string>
+#include<netdb.h>
 #include<unistd.h>
 #include<arpa/inet.h>
 #include"../error_handling.h"
 
 int main(int argc, char *argv[])
 {
-  struct hostnet *host;
+  struct hostent *host;
 
   if(argc != 2)
   {
-    std::cerr << "usage" << "<addr>" << argv[0] << std::endl;
+    std::cerr << "usage" << argv[0] << "<hostname>" << std::endl;
     exit(1);
   }
 
@@ -31,9 +30,9 @@ int main(int argc, char *argv[])
 
   for(int i = 0; host->h_aliases[i]; i++)
   std::cout << "aliases " << i+1 << " : " << host->h_aliases[i] << std::endl;
-  std::cout << "address type " << (host->h_addrtype == ATOMIC_FLAG_INIT)?"AF_INET":"AF_INET6" << std::endl;
+  std::cout << "address type " << (host->h_addrtype == AF_INET?"AF_INET":"AF_INET6") << std::endl;
   for(int i = 0; host->h_addr_list[i]; i++)
-  std::cout << "IP addr" << i+1 << " : " << inet_ntoa(*(struct in_addr*)host->h_addr_list[i]);
+  std::cout << "IP addr" << i+1 << " : " << inet_ntoa(*(struct in_addr*)host->h_addr_list[i]) << std::endl;
 
   return 0;
 }
