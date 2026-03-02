e2fdd54f1b61c32c9f6d4ff36f68af95ca590486
epqlffltm <kjh55984249@gmail.com>
2026-03-02 18:26:38 +0900

    therad1

--- src/thread/thread1.cpp
+++ src/thread/thread1.cpp
@@ -7,13 +7,13 @@ thread
 #include<thread>
 #include<unistd.h>
 
-void th(void id);
+void th(int id);
 
 int main(int argc, char *arcv[])
 {
-  std::thread.t1(th);
-  std::thread.t2(th);
-  std::thread.t3(th);
+  std::thread t1(th);
+  std::thread t2(th);
+  std::thread t3(th);
 
   t1.join();
   t2.join();
@@ -22,7 +22,7 @@ int main(int argc, char *arcv[])
   return 0;
 }
 
-void th(void id)
+void th(int id)
 {
   for(int i = 0; i < 10; i++)
   {
